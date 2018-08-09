e464fa1c02ddf4db37206dcf6e74af9c4774a624
jtatarow <john.tatarowicz@gmail.com>
2018-08-09 00:22:29 -0400

    Working UNet implemented

--- src/models/unet.py
+++ src/models/unet.py
@@ -1,44 +1,53 @@
 import tensorflow as tf
-from tensorflow.layers import conv2d, max_pooling2d, conv2d_transpose
+from tensorflow.layers import conv2d, max_pooling2d, conv2d_transpose, dropout
+from tensorflow.python.ops import array_ops
 
 
 class UNet:
     def __init__(self):
         pass
 
-    def conv_block(self, input, filters):
-        conv1 = conv2d(input, filters, kernel_size=(3,3), strides=(1, 1), activation=tf.nn.relu)
-        conv2 = conv2d(conv1, filters, kernel_size=(3,3), strides=(1, 1), activation=tf.nn.relu)
-        pool1 = max_pooling2d(conv2, pool_size=(2, 2), strides=(2, 2))
+    def conv_block(self, input, filters, scope):
+        with tf.variable_scope(scope):
+            conv1 = conv2d(input, filters, kernel_size=(3,3), activation=tf.nn.relu, padding='SAME')
+            conv2 = conv2d(conv1, filters, kernel_size=(3,3), activation=tf.nn.relu, padding='SAME')
 
-        return pool1
+        return conv2
 
-    def deconv_block(self, input, filters, conv):
-        deconv1 = conv2d_transpose(input, filters, kernel_size=(3, 3), strides=(2, 2))
-        deconv_shape = tf.shape(deconv1)
-        conv_shape = tf.shape(conv)
-        offsets = [0, (conv_shape[1]-deconv_shape[1])//2, (conv_shape[2]-deconv_shape[2])//2, 0]
-        size = [-1, deconv_shape[1], deconv_shape[2], filters*2]
-        conv_crop = tf.slice(conv, offsets, size)
-        conv1 = tf.concat([deconv1, conv_crop], 3)
-
-        conv2 = conv2d(conv1, filters, kernel_size=(3,3), activation=tf.nn.relu)
-        conv3 = conv2d(conv2, filters, kernel_size=(3,3), activation=tf.nn.relu)
+    def deconv_block(self, input, filters, conv, padding, scope):
+        with tf.variable_scope(scope):
+            deconv1 = conv2d_transpose(input, filters, kernel_size=(3, 3), strides=[2, 2], padding=padding)
+            deconv_shape = tf.shape(deconv1)
+            conv_shape = tf.shape(conv)
+            offsets = [0, (conv_shape[1]-deconv_shape[1])//2, (conv_shape[2]-deconv_shape[2])//2, 0]
+            size = [-1, deconv_shape[1], deconv_shape[2], filters]
+            conv_crop = tf.slice(conv, offsets, size)
+            conv1 = tf.concat([deconv1, conv_crop], 3)
+            drop = dropout(conv1, .25)
+            conv2 = conv2d(drop, filters, kernel_size=(3,3), activation=tf.nn.relu, name='middle1', padding="SAME")
+            conv3 = conv2d(conv2, filters, kernel_size=(3,3), activation=tf.nn.relu, name='middle2', padding="SAME")
 
         return conv3
 
     def build(self, input):
-        conv1 = self.conv_block(input, 64)
-        conv2 = self.conv_block(conv1, 128)
-        conv3 = self.conv_block(conv2, 256)
-        conv4 = self.conv_block(conv3, 512)
+        self.conv1 = self.conv_block(input, 64, "conv1")
+        pool1 = max_pooling2d(self.conv1, pool_size=(2, 2), strides=2)
+        drop1 = dropout(pool1, .25)
+        self.conv2 = self.conv_block(drop1, 128, 'conv2')
+        pool2 = max_pooling2d(self.conv2, pool_size=(2, 2), strides=2)
+        drop2 = dropout(pool2, .25)
+        self.conv3 = self.conv_block(drop2, 256, 'conv3')
+        pool3 = max_pooling2d(self.conv3, pool_size=(2, 2), strides=2)
+        drop3 = dropout(pool3, .25)
+        self.conv4 = self.conv_block(drop3, 512, 'conv4')
+        pool4 = max_pooling2d(self.conv4, pool_size=(2, 2), strides=2)
+        drop4 = dropout(pool4, .25)
 
-        conv5 = conv2d(conv4, 1024, kernel_size=(3, 3), strides=(1, 1), activation=tf.nn.relu)
-        conv5 = conv2d(conv5, 1024, kernel_size=(3, 3), strides=(1, 1), activation=tf.nn.relu)
+        self.conv5 = conv2d(drop4, 1024, kernel_size=(3, 3), activation=tf.nn.relu, padding='SAME')
+        self.conv5_2 = conv2d(self.conv5, 1024, kernel_size=(3, 3), activation=tf.nn.relu, padding='SAME')
 
-        deconv4 = self.deconv_block(conv5, 512, conv4)
-        deconv3 = self.deconv_block(deconv4, 256, conv3)
-        deconv2 = self.deconv_block(deconv3, 128, conv2)
-        deconv1 = self.deconv_block(deconv2, 64, conv1)
-
-        self.output = conv2d(deconv1, filters=2, kernel_size=1)
+        self.deconv4 = self.deconv_block(self.conv5_2, 512, self.conv4, "SAME", 'deconv4')
+        self.deconv3 = self.deconv_block(self.deconv4, 256, self.conv3, "VALID", 'dconv3')
+        self.deconv2 = self.deconv_block(self.deconv3, 128, self.conv2, "SAME", 'deconv2')
+        self.deconv1 = self.deconv_block(self.deconv2, 64, self.conv1, "VALID", 'deconv1')
+        self.output = conv2d(self.deconv1, filters=1, kernel_size=1, name='logits')
---
+++ unet_train.py
@@ -0,0 +1,110 @@
+import tensorflow as tf
+import numpy as np
+import warnings
+import random
+import os
+import sys
+from src.models.unet import UNet
+
+from tqdm import tqdm
+from itertools import chain
+from skimage.io import imread, imshow, imread_collection, concatenate_images
+from skimage.transform import resize
+from skimage.morphology import label
+
+warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
+
+seed = 42
+random.seed = seed
+np.random.seed = seed
+
+# train_filename_dataset = tf.data.Dataset.list_files('data/train/images/*.png')
+# test_filename_dataset = tf.data.Dataset.list_files('data/test/images/*.png')
+#
+# train_images = train_filename_dataset.map(lambda x: tf.image.decode_png(tf.read_file(x)))
+# test_images = test_filename_dataset.map(lambda x: tf.image.decode_png(tf.read_file(x)))
+#
+# iterator = train_images.make_one_shot_iterator()
+# next_image = iterator.get_next()
+
+IMG_SIZE = 101
+IMG_CHANNELS = 3
+path_train = 'data/train'
+path_test = 'data/test'
+
+train_ids = next(os.walk(path_train + "/images"))[2]
+test_ids = next(os.walk(path_test + "/images"))[2]
+
+train_images = np.zeros((len(train_ids), IMG_SIZE, IMG_SIZE, IMG_CHANNELS), dtype=np.uint8)
+train_labels = np.zeros((len(train_ids), IMG_SIZE, IMG_SIZE, 1), dtype=np.bool)
+
+print('Getting and resizing train images and masks without padding ... ')
+sys.stdout.flush()
+for n, id_ in tqdm(enumerate(train_ids), total=len(train_ids)):
+    img = imread(path_train + "/images/" + id_)[:,:,:IMG_CHANNELS]
+    train_images[n] = img
+
+    mask = imread(path_train + "/masks/" + id_)
+    mask = np.expand_dims(mask, axis = -1)
+
+    train_labels[n] = mask
+
+test_images = np.zeros((len(test_ids), IMG_SIZE, IMG_SIZE, IMG_CHANNELS), dtype=np.uint8)
+for n, id_ in tqdm(enumerate(test_ids), total=len(test_ids)):
+    img = imread(path_test + "/images/" + id_)[:,:,:IMG_CHANNELS]
+    test_images[n] = img
+
+def shuffle():
+    global images, labels
+
+    p = np.random.permutation(len(train_ids))
+    images = train_images[p]
+    labels = train_labels[p]
+
+def next_train_batch(batch_s, batch_count, is_first_iter):
+    if(batch_count == 0):
+        shuffle()
+    count = batch_s * batch_count
+    return images[count:(count + batch_s)], labels[count:(count + batch_s)]
+
+tf.reset_default_graph()
+X = tf.placeholder(tf.float32, [None, IMG_SIZE, IMG_SIZE, 3])
+Y_ = tf.placeholder(tf.float32, [None, IMG_SIZE, IMG_SIZE, 1])
+lr = tf.placeholder(tf.float32)
+#
+model = UNet()
+model.build(X)
+cross_entropy = tf.losses.sigmoid_cross_entropy(Y_, model.output)
+train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+batch_count = 0
+display_count = 1
+
+for i in range(12000):
+    # training on batches of 50 images with 50 mask images
+    if (batch_count > 79):
+        batch_count = 0
+
+    batch_X, batch_Y = next_train_batch(50, batch_count, i == 0)
+
+    batch_count += 1
+
+    train_loss, _ = sess.run([cross_entropy, train_step], {X: batch_X, Y_: batch_Y, lr: 0.0005})
+
+    if (i % 200 == 0):
+        print(str(display_count) + " training loss:" + str(train_loss))
+        display_count += 1
+
+print("Done!")
+# epochs = 3
+
+#
+# with tf.Session() as sess:
+#     for i in range(10):
+#         image_array = sess.run([next_image])
+#         print(image_array)
